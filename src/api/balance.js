219dada3ec05aa13040c25def4c766ce882ecb40
orenys7 <orenys7@gmail.com>
2020-12-26 19:55:59 +0200

    ROIC calculation

--- src/api/balance.js
+++ src/api/balance.js
@@ -7,81 +7,103 @@ router.get("/:quote", async (req, res, next) => {
     const quote = req.params.quote;
     const incomeUrl = `https://www.marketwatch.com/investing/stock/${quote}/financials?mod=mw_quote_tab`;
     const balanceUrl = `https://www.marketwatch.com/investing/stock/${quote}/financials/balance-sheet`;
+    const cashFlowUrl = `https://www.marketwatch.com/investing/stock/${quote}/financials/cash-flow`;
 
     const categories = {
         sales: 0,
+        eps: 48,
+        equity: 77,
+        netOperatingCashFlow: 15,
+
         netIncome: 36,
-        totalAssets: 35,
-        totalLiabilites: 60,
-        shareholdersEquity: 75,
-        equity: 78,
-        eps: 45,
+        dividends: 33,
+        currentDebt: 38,
+        longTermDebt: 47,
     };
 
     const incomeHtml = await fetchData(incomeUrl);
     const balanceHtml = await fetchData(balanceUrl);
-    
+    const cashFlowHtml = await fetchData(cashFlowUrl);
+
     const sales = getStats(incomeHtml.data, categories.sales);
     const growthSales = growthRate(sales);
-    const netIncome = getStats(incomeHtml.data, categories.netIncome);
-    const growthNetIncome = growthRate(netIncome);
     const eps = getStats(incomeHtml.data, categories.eps);
     const growthEps = growthRate(eps);
-    const totalAssets = getStats(balanceHtml.data, categories.totalAssets);
-    const growthTotalAssets = growthRate(totalAssets);
-    const totalLiabilites = getStats(
-        balanceHtml.data,
-        categories.totalLiabilites
-    );
-    const growthTotalLiabilites = growthRate(totalLiabilites);
-    const shareholdersEquity = getStats(
-        balanceHtml.data,
-        categories.shareholdersEquity
-    );
-    const growthShareholdersEquity = growthRate(shareholdersEquity);
     const equity = getStats(balanceHtml.data, categories.equity);
     const growthEquity = growthRate(equity);
+    const netOperatingCashFlow = getStats(
+        cashFlowHtml.data,
+        categories.netOperatingCashFlow
+    );
+    const growthNetOperatingCashFlow = growthRate(netOperatingCashFlow);
 
-    console.log();
+    const netIncome = getStats(incomeHtml.data, categories.netIncome);
+    const dividends = getStats(cashFlowHtml.data, categories.dividends);
+    const currentDebt = getStats(balanceHtml.data, categories.currentDebt);
+    const longTermDebt = getStats(balanceHtml.data, categories.longTermDebt);
+    const totalDebt = calculateDebt(currentDebt, longTermDebt);
+
+    const roic = calculateRoic(netIncome, dividends, totalDebt, equity);
 
     res.send({
-        sales,
-        growthSales,
-        eps,
-        growthEps,
-        netIncome,
-        growthNetIncome,
-        equity,
-        growthEquity,
-        shareholdersEquity,
-        growthShareholdersEquity,
-        totalAssets,
-        growthTotalAssets,
-        totalLiabilites,
-        growthTotalLiabilites
+        balance: {
+            roic,
+            sales,
+            eps,
+            equity,
+            netOperatingCashFlow,
+        },
+        growth: {
+            sales: growthSales,
+            eps: growthEps,
+            equity: growthEquity,
+            netOperatingCashFlow: growthNetOperatingCashFlow,
+        },
     });
 });
 
 function encodeValues(values) {
-    return values
-        .split(":")[1]
-        .replace("}", "")
-        .replace("[", "")
-        .replace("]", "")
-        .split(",");
+    const vals = values.split(",");
+    return vals.map((val) =>
+        Number.parseFloat(val) ? Number.parseFloat(val) : 0.0
+    );
+}
+
+function calculateDebt(currentDebt, longTermDebt) {
+    const totalDebt = [];
+    currentDebt.map((val, index) => totalDebt.push(val + longTermDebt[index]));
+    return totalDebt;
+}
+
+function calculateRoic(netIncome, dividends, totalDebt, equity) {
+    const roic = [];
+    netIncome.map((income, index) =>
+        roic.push(
+            ((income - dividends[index]) /
+                (totalDebt[index] + equity[[index]])) *
+                100
+        )
+    );
+    return roic;
 }
 
 function growthRate(rateValues) {
     const growthRateValues = [];
-    rateValues.forEach((rateVal) => {
-        growthRateValues.push((rateVal - rateValues[0]) / Math.abs(rateValues[0]) * 100);
+    rateValues.map((rateVal, index) => {
+        growthRateValues.push(
+            ((rateVal - rateValues[index - 1]) /
+                Math.abs(rateValues[index - 1])) *
+                100
+        );
     });
     return growthRateValues;
 }
 
 function getStats(html, index) {
     const $ = cheerio.load(html);
-    const statsTable = $(".miniGraph")[index].attribs["data-chart"];
+    const statsTable = $(".chart--financials")[index].attribs[
+        "data-chart-data"
+    ];
     return encodeValues(statsTable);
 }
 
